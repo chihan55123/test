3abd0cdcc463005d262f10a6566485ebfa5576a4
chihan55123 <SKYsky5512300@gmail.com>
2020-09-23 11:24:48 +0800

    first commit

---
+++ src/Click.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import {Alert, Button} from 'react-native';
+
+
+export default function Click() {
+
+
+
+const [count, setCount] = useState(0);
+
+let countString = "count:"+count;
+
+
+
+function handleClick() {
+
+ setCount(count+1);
+
+ Alert.alert("count:"+count);
+
+}
+
+
+
+return (
+
+  <Button title={countString} onPress={handleClick}/>
+
+);
+
+}
+
+
+
+
