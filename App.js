c9200d416c94152700477aec1564d4781be65506
chihan55123 <SKYsky5512300@gmail.com>
2020-09-23 12:00:58 +0800

    asdasdadasdsad

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import Click from './src/Click';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>qweqweqw</Text>
       <Click/>
       <StatusBar style="auto" />
     </View>
